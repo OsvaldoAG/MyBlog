d2556581b25ce948ddc50e8ece18c027f1079e4f
OsvaldoAG <osvaldo_1320114000@uptecamac.edu.mx>
2022-01-27 18:44:13 -0600

    blog

---
+++ resources/views/template/index.blade.php
@@ -0,0 +1,103 @@
+<!DOCTYPE HTML>
+<!--
+	Strata by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>My Blog OAG</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="/css/main.css" />
+	</head>
+	<body class="is-preload">
+
+		<!-- Header -->
+			<header id="header">
+				<div class="inner">
+					<a href="#" class="image avatar"><img src="images/avatar.jpg" alt="" /></a>
+					<h1><strong>Osvaldo Aviles Guzman</strong><br />
+					Y este es mi BLOG<br />
+				</div>
+			</header>
+
+		<!-- Main -->
+			<div id="main">
+
+				<!-- One -->
+					<section id="one">
+						<header class="major">
+							<h2>Soy un estudiante de la UPT y les mostrare un poco de mi y de mis cosas favoritas<br /></h2>
+						</header>
+						<p><br>Tengo 19 años</br><br>Actualmente estudio la carrera de Ingenieria en Software</br><br>Soy mu Sociable</br><br>Me gusta el deporte</br><br>Me gusta todo lo que nesesite un procesador</br></p>
+						<ul class="actions">
+							<li><a href="#" class="button">Precentacion</a></li>
+						</ul>
+					</section>
+
+				<!-- Two -->
+					<section id="two">
+						<h2>Mis hobbies</h2>
+						<div class="row">
+							<article class="col-6 col-12-xsmall work-item">
+								<a href="images/fulls/01.jpg" class="image fit thumb"><img src="images/thumbs/01.jpg" alt="" /></a>
+								<h3>Mountain Bike</h3>
+								<p>llevo 1 año practicando el mountan bike, esto es una rama del ciclismo solo que este se desarrolla o se concentra mas en una montaña o cerro, con caminos de tierra, caminos irregulares, obtaculos, que ponen en prueba tu destresa y agilidad, he competido en carreras de 40, 35 y 60 KM, a los cuales he salido en primero y segundo lugar</p>
+							</article>
+							<article class="col-6 col-12-xsmall work-item">
+								<a href="images/fulls/02.jpg" class="image fit thumb"><img src="images/thumbs/02.jpg" alt="" /></a>
+								<h3>Futbol</h3>
+								<p>Actualmente juego en 3 equipos de futbol, uno con mis familiares de futbol llanero, otro en una liga de futbol 7, y optro que es un nivel semi profecional el cual he lllegado a jugar en el estadio azteca</p>
+							</article>
+							<article class="col-6 col-12-xsmall work-item">
+								<a href="images/fulls/03.jpg" class="image fit thumb"><img src="images/thumbs/03.jpg" alt="" /></a>
+								<h3>Carros</h3>
+								<p>Me gustan los carros, podria decirse que hasta los amo, me gutan mucho el ver videos y todo lo que se le puede hacer a un auto, como modificaciones</p>
+							</article>
+							<article class="col-6 col-12-xsmall work-item">
+								<a href="images/fulls/04.jpg" class="image fit thumb"><img src="images/thumbs/04.jpg" alt="" /></a>
+								<h3>Entretenimiento</h3>
+								<p>Me gusta jugar videojuegos he partisipado en torneos a nivel nacional y nunca he pasado de el 10cimo lugar, pero en fin me gustan como entretenimiento no una dedicacion</p>
+							</article>
+							<article class="col-6 col-12-xsmall work-item">
+								<a href="images/fulls/05.jpg" class="image fit thumb"><img src="images/thumbs/05.jpg" alt="" /></a>
+								<h3>Computacion</h3>
+								<p>Me gusta todo lo que tenga que ver con computadoras, y mas ver videos de PC gamers, el como las arman es una satisfaccion y el como saber que componentes pueden rendir mas a la misma</p>
+							</article>
+							<article class="col-6 col-12-xsmall work-item">
+								<a href="images/fulls/06.jpg" class="image fit thumb"><img src="images/thumbs/06.jpg" alt="" /></a>
+								<h3>Vida social</h3>
+								<p>Soy muy social, me gusta salir a divertirme con mi familia o amigos, y combinar los carros con salidas con amigos a lugares</p>
+							</article>
+						</div>
+						<ul class="actions">
+							<li><a href="#" class="button">Regresar a precentacion</a></li>
+						</ul>
+					</section>
+
+				<!-- Three -->
+				<section id="three">
+						<h2>Mis fotos</h2>
+						<p>Imagenes de mi deporte </p>
+						<div class="row">
+							<article class="col-6 col-12-xsmall work-item">
+								<input type="image" src="css/images/IMG1.jpeg" alt="" width="500" height="300">
+								<input type="image" src="css/images/IMG2.jpeg" alt="" width="500" height="500">
+								<input type="image" src="css/images/IMG3.jpeg" alt="" width="500" height="650">
+							</article>		
+						</div>
+					</section>
+
+			</div>
+			
+		<!-- Scripts -->
+			<script src="/js/jquery.min.js"></script>
+			<script src="/js/jquery.poptrox.min.js"></script>
+			<script src="/js/browser.min.js"></script>
+			<script src="/js/breakpoints.min.js"></script>
+			<script src="/js/util.js"></script>
+			<script src="/js/main.js"></script>
+
+	</body>
+</html>
